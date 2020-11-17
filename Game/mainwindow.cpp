f775acc53a061ba8656ff1c753c7546f02e40555
Duc Hong <duc.hong@tuni.fi>
2020-11-17 23:01:08 +0200

    init mainwindow

--- Game/mainwindow.cpp
+++ Game/mainwindow.cpp
@@ -1,14 +1,60 @@
 #include "mainwindow.hh"
 #include "ui_mainwindow.h"
 
+const int PADDING = 10;
+
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+  ui->gameView->setFixedSize(width_, height_);
+  ui->centralwidget->setFixedSize(width_ + ui->startButton->width() + PADDING, height_ + PADDING);
+
+  ui->startButton->move(width_+ PADDING, PADDING);
+
+  map =new QGraphicsScene(this);
+  ui->gameView->setScene(map);
+  map->setSceneRect(0,0,width_, height_);
+  resize(minimumSizeHint());
+
+  timer = new QTimer(this);
+
 }
 
 MainWindow::~MainWindow()
 {
   delete ui;
+}
+
+void MainWindow::setSize(int w, int h)
+{
+  width_ = w;
+  height_ = h;
+}
+
+void MainWindow::setTick(int t)
+{
+
+}
+
+void MainWindow::addActor(int locX, int locY, int type)
+{
+
+}
+
+void MainWindow::updateCoords(int nX, int nY)
+{
+
+}
+
+void MainWindow::setPicture(QImage &img)
+{
+  map->setBackgroundBrush(img);
+}
+
+
+void MainWindow::onStartButtonClicked()
+{
+
 }
--- Game/mainwindow.hh
+++ Game/mainwindow.hh
@@ -1,7 +1,14 @@
-#ifndef MAINWINDOW_HH
+﻿#ifndef MAINWINDOW_HH
 #define MAINWINDOW_HH
 
 #include <QMainWindow>
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QTimer>
+#include <iostream>
+#include <memory>
+#include <QVector>
+#include <map>
 
 namespace Ui {
   class MainWindow;
@@ -12,11 +19,32 @@ class MainWindow : public QMainWindow
   Q_OBJECT
 
 public:
-  explicit MainWindow(QWidget *parent = nullptr);
+  explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
+
+  void setSize(int w, int h);
+  void setTick(int t);
+
+  void addActor(int locX, int locY, int type=0);
+  void updateCoords(int nX, int nY);
+  void setPicture(QImage &img);
+
+signals:
+  void gameStarted();
+
+private slots:
+  void onStartButtonClicked();
 
 private:
   Ui::MainWindow *ui;
+  QGraphicsScene *map;
+  QTimer *timer;
+  QVector<QGraphicsItem*> actors_;
+  // ActorItem last_;
+
+  int width_ = 500;
+  int height_ = 500;
+  int tick_ = 500;
 };
 
 #endif // MAINWINDOW_HH
